e228c3b8b08646019c8543cb814fdeae035c7f6c
h2sm <68076283+h2sm@users.noreply.github.com>
2020-12-15 22:58:43 +0300

    Update lenin.java

--- lenin.java
+++ lenin.java
@@ -10,48 +10,63 @@ public class textAnalysis {
         String filename = "D:/Java_laba/lenin_laba/lenin.txt";
         List<String> words = getWords(filename);
         LinkedHashSet<String> x = new First().withoutCapital(words);
-        new First().decreasingLength(x);
+        //new First().decreasingLength(x);
         new Second().countUniqueWords(words);
     }
-    private static List<String> getWords(String filename) throws IOException{
+
+    private static List<String> getWords(String filename) throws IOException {
         var splitter = Pattern.compile("[\\p{Punct}\\d\\s«…»–]+");
         return Files.lines(Path.of(filename))
-                    .flatMap(splitter::splitAsStream)
-                    .filter(w -> ! w.isEmpty())
-                    .collect(Collectors.toList());
+                .flatMap(splitter::splitAsStream)
+                .filter(w -> !w.isEmpty())
+                .collect(Collectors.toList());
     }
-    static class First {
-        LinkedHashSet<String> withoutCapital(List<String> words) throws IOException {
-            LinkedHashSet<String> strings = new LinkedHashSet<>();
-            //String file_output = "D:/Java_laba/lenin_laba/out.txt";
-            for (String a : words) {
-                String end = a.toLowerCase(Locale.getDefault());
-                strings.add(end);
-            }
-            //System.out.println(strings); выводить в файл
-            return strings;
-        }
 
-        void decreasingLength(LinkedHashSet<String> strings) {
-            //SortedSet<String> sortedSet = new TreeSet<>(String::length);
-            SortedSet<String> sortedSet = new TreeSet<>(String::compareToIgnoreCase);//компаратор который соритрует в лексикограф.порядке
-            sortedSet.addAll(strings);
-            //вывести в файл
-            System.out.println(sortedSet);
+    private static void writeResultToFile(String filename, List<String>
+            lines) throws IOException {
+        Files.write(Path.of(filename), lines);
+    }
+}
+class First {
+    LinkedHashSet<String> withoutCapital(List<String> words) throws IOException {
+        LinkedHashSet<String> strings = new LinkedHashSet<>();
+            //String file_output = "D:/Java_laba/lenin_laba/out.txt";
+        for (String a : words) {
+            String end = a.toLowerCase(Locale.getDefault());
+            strings.add(end);
         }
-        void decreasingLengthLimited(SortedSet<String> sortedSet){
-            TreeSet<String> sortedSet2 = new TreeSet<>();
-            //TreeSet<String> sortedSet1 = new TreeSet<String>(Comparator.comparing(String::length));
-            for (String i : sortedSet){
-                if (i.length()>=4&&i.length()<=7) sortedSet2.add(i);
-            }
+            //System.out.println(strings); выводить в файл
+        return strings;
+    }
+
+    void decreasingLength(LinkedHashSet<String> strings) {
+            //SortedSet<String> sortedSet = new TreeSet<>(String::length);
+        SortedSet<String> sortedSet = new TreeSet<>(String::compareToIgnoreCase);//компаратор который соритрует в лексикограф.порядке
+        sortedSet.addAll(strings);
+            //вывести в файл
+        System.out.println(sortedSet);
+    }
+    void decreasingLengthLimited(SortedSet<String> sortedSet){
+        TreeSet<String> sortedSet2 = new TreeSet<>();
+        //TreeSet<String> sortedSet1 = new TreeSet<String>(Comparator.comparing(String::length));
+        for (String i : sortedSet){
+            if (i.length()>=4&&i.length()<=7) sortedSet2.add(i);
         }
     }
 }
+
 class Second{
     void countUniqueWords(List<String> words){
+        String filename = "D:/Java_laba/lenin_laba/output.txt";
+        Set<String> wordsSet = new HashSet<String>(words);
+        for(String s: wordsSet){
+          System.out.println(s + " " + Collections.frequency(wordsSet,s));
+        }
+    }
+    void countUniqueWordsButMap(List<String> words){
 
     }
 }
+
 
 
