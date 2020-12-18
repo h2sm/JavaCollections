2f36a32dae00be0c307feb0d888e7f7d0c9958fd
h2sm <68076283+h2sm@users.noreply.github.com>
2020-12-18 18:17:32 +0300

    Update lenin.java

--- lenin.java
+++ lenin.java
@@ -7,30 +7,19 @@ import java.util.stream.Collectors;
 
 public class textAnalysis {
     public static void main(String[] args) throws IOException {
-        String filename = "D:/Java_laba/lenin_laba/lenin.txt";
-        List<String> words = getWords(filename);
+        new Lenin();//а где старый
+        List<String> words = Lenin.getWords();
         LinkedHashSet<String> x = new First().withoutCapital(words);
         //new First().decreasingLength(x);
-        new Second().countUniqueWords(words);
-    }
-
-    private static List<String> getWords(String filename) throws IOException {
-        var splitter = Pattern.compile("[\\p{Punct}\\d\\s«…»–]+");
-        return Files.lines(Path.of(filename))
-                .flatMap(splitter::splitAsStream)
-                .filter(w -> !w.isEmpty())
-                .collect(Collectors.toList());
-    }
-
-    private static void writeResultToFile(String filename, List<String>
-            lines) throws IOException {
-        Files.write(Path.of(filename), lines);
+        new Second();
+        TreeMap<String,Integer> treeMap = Second.countUniqueWordsButMap(words);
+        TreeMap<String, Integer> trmap = Second.countUniqueWordsButMapLimited(treeMap);
+        Second.countUniqueWordsButMapLimitedLexicographical(trmap);
     }
 }
 class First {
     LinkedHashSet<String> withoutCapital(List<String> words) throws IOException {
         LinkedHashSet<String> strings = new LinkedHashSet<>();
-            //String file_output = "D:/Java_laba/lenin_laba/out.txt";
         for (String a : words) {
             String end = a.toLowerCase(Locale.getDefault());
             strings.add(end);
@@ -48,7 +37,7 @@ class First {
     }
     void decreasingLengthLimited(SortedSet<String> sortedSet){
         TreeSet<String> sortedSet2 = new TreeSet<>();
-        //TreeSet<String> sortedSet1 = new TreeSet<String>(Comparator.comparing(String::length));
+        TreeSet<String> sortedSet1 = new TreeSet<String>(Comparator.comparing(as -> as.length()));
         for (String i : sortedSet){
             if (i.length()>=4&&i.length()<=7) sortedSet2.add(i);
         }
@@ -57,16 +46,52 @@ class First {
 
 class Second{
     void countUniqueWords(List<String> words){
-        String filename = "D:/Java_laba/lenin_laba/output.txt";
         Set<String> wordsSet = new HashSet<String>(words);
         for(String s: wordsSet){
           System.out.println(s + " " + Collections.frequency(wordsSet,s));
         }
     }
-    void countUniqueWordsButMap(List<String> words){
-
+    static TreeMap<String,Integer> countUniqueWordsButMap(List<String> words){//2а
+        TreeMap<String,Integer> treeMap = new TreeMap<>();//слово - количество использований, treeset!!
+        for (String s:words){
+            if (!treeMap.containsKey(s)) treeMap.put(s,Collections.frequency(words,s));
+            //System.out.println(treeMap);
+        }
+        return treeMap;
+    }
+    static TreeMap<String, Integer> countUniqueWordsButMapLimited(TreeMap<String,Integer>treeMap){//от 4 до 7
+        TreeMap<String,Integer> treeMap1 = new TreeMap<>();
+        for (int i = 0; i <treeMap.size() ; i++) {
+            String key = (String) treeMap.keySet().toArray()[i];
+            Integer value = treeMap1.get(i);
+            if (key.length()>=4 &&key.length()<=7) treeMap1.put(key,value);
+        }
+        return treeMap1;
+    }
+    static void countUniqueWordsButMapLimitedLexicographical(TreeMap<String,Integer>treeMap){
+        //SortedMap<String, Integer> sortedMap = new SortedMap<String, Integer>();
+        TreeMap<String,Integer> treeMap1 = new TreeMap<>(String::compareToIgnoreCase);
     }
 }
+class Three{
+    void mapping(List<String> words){
+        
+    }
+}
+class Lenin {
 
+    static List<String> getWords() throws IOException {
+        String filename = "D:/Java_laba/lenin_laba/lenin.txt";
+        var splitter = Pattern.compile("[\\p{Punct}\\d\\s«…»–]+");
+        return Files.lines(Path.of(filename))
+                .flatMap(splitter::splitAsStream)
+                .filter(w -> ! w.isEmpty())
+                .collect(Collectors.toList());
+    }
+
+    static void writeResultToFile(String filename, List<String> lines) throws IOException {
+        Files.write(Path.of(filename), lines);
+    }
+}
 
 
